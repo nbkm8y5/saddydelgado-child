5c5e609fa3f8b3e8dc797d290029ee94bac026d8
rmore071 <rmore071@fiu.edu>
2016-06-04 10:07:01 -0400

    Even more changes specifically to Main Menu

--- footer.php
+++ footer.php
@@ -17,9 +17,14 @@
   		<?php get_sidebar( 'footer-one' ); ?>
   		<?php get_sidebar( 'footer-two' ); ?>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'saddydelgado' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'saddydelgado' ), 'WordPress' ); ?></a>
+			<p>1430 S. Dixie Highway, Suite 110, Coral Gables FL 33146 <span>•</span>
+		  Cell: 305.632.4256 <span>•</span> Office: 305.666.0562 <span>•</span>
+		  Fax 305.666.4187</p>
+		  <p id="disclaimer">This offering is subject to errors, omissions, prior sale, change of price, or withdrawal without notice. Not intended to solicit currently listed property.</p>
+			<p><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'saddydelgado' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'saddydelgado' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'saddydelgado' ), 'saddydelgado', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s', 'saddydelgado' ), 'saddydelgado', '<a href="http://greasyhacks.com/" rel="designer">greasyhacks.com</a>' ); ?></p>
+			<img id="footer_logo" src="http://localhost:8888/dev_sd/wp-content/uploads/2016/06/footer_logo.jpg">
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
