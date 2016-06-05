7cfcf27f742b454f2e3cffe819a758c849684948
rmore071 <rmore071@fiu.edu>
2016-06-05 19:16:50 -0400

    Corrected mobile menu button.

--- header.php
+++ header.php
@@ -43,7 +43,12 @@
 			endif; ?>
 		</div><!-- .site-branding -->
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'saddydelgado' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<!-- <?php esc_html_e( 'Primary Menu', 'saddydelgado' ); ?> -->
+				<span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+			</button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
